1239e9f6671dcd64a0a319814493551943dedbb1
AntonioMarcel <marcel.sotero@gmail.com>
2026-04-02 11:32:40 -0300

    feat: adiciona config.py com seletores e URL base

---
+++ scraper/config.py
@@ -0,0 +1,9 @@
+BASE_URL = "https://legislacao.presidencia.gov.br/#"
+
+SELECTORS = {
+    "cards": "div.card.p-2.pr-3.pl-3.w-100",
+    "titulo": "h4.card-title a",
+    "status": "p.card-category",
+    "ementa": "p.pt-2",
+    "botoes": "ul.list-inline li a.btn",
+}
---
+++ scraper/parser.py
@@ -0,0 +1,5 @@
+from playwright.async_api import Page
+
+class Parser:
+    async def parse(self, page: Page):
+        pass
